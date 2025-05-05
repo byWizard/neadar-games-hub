f5fc042dc295260cae1a8c405c9078f6f656a8be
byWizard <90400132+byWizard@users.noreply.github.com>
2025-05-05 20:06:54 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,12 @@
+// config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDhMfbhd7emAXNKDexXxaCxZ0k2DfkRcVg",
+  authDomain: "my-games-app-hub.firebaseapp.com",
+  databaseURL: "https://my-games-app-hub-default-rtdb.firebaseio.com",
+  projectId: "my-games-app-hub",
+  storageBucket: "my-games-app-hub.appspot.com",
+  messagingSenderId: "251367004030",
+  appId: "1:251367004030:web:2b1be1b1c76ee80c0d052f"
+};
+
+export const RAWG_API_KEY = "48b79844fcc44af7860a5fa89de88ca8";
