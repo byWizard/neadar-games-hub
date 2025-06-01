71bb66a82aa66ee0fc25def14e708f356fb3ba4d
byWizard <90400132+byWizard@users.noreply.github.com>
2025-06-01 22:58:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -500,3 +500,17 @@ function updateParticleColor(theme) {
 // === Инициализация ===
 createParticles();
 animateParticles();
+
+// === твич === //
+<!-- Add a placeholder for the Twitch embed -->
+<div id="twitch-embed"></div>
+
+<!-- Load the Twitch embed script -->
+<script src="https://player.twitch.tv/js/embed/v1.js"></script>
+
+<!-- Create a Twitch.Player object. This will render within the placeholder div -->
+<script type="text/javascript">
+  new Twitch.Player("twitch-embed", {
+    video: "2474205687"
+  });
+</script>
